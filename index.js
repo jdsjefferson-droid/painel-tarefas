74219288fb9ceb7dbf35284728b4c2f0e03b450c
jdsjefferson-droid <jdsjefferson@gmail.com>
2026-03-20 16:22:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 export default function Home() {
   const tarefas = [
     {unidade:"CAPS 2", tarefa:"Instalar roteador", tecnico:"", status:"pendente"},
@@ -13,15 +12,15 @@ export default function Home() {
   };
 
   return (
-    <div style={{padding:20,fontFamily:"Arial"}}>
-      <h1 style={{fontSize:40}}>Painel da equipe</h1>
+    <div style={{padding:20,fontFamily:"Arial",background:"#f4f5f7"}}>
+      <h1 style={{fontSize:60,marginBottom:30}}>Painel da equipe</h1>
 
       {tarefas.map((t,i)=>(
-        <div key={i} style={{background:"#fff",margin:"10px 0",padding:15,borderLeft:"8px solid blue"}}>
-          <div style={{fontSize:22,fontWeight:"bold"}}>{t.unidade}</div>
-          <div>{t.tarefa}</div>
-          <div>{t.tecnico || "-"}</div>
-          <div style={{background:cores[t.status],color:"#fff",padding:8,marginTop:5}}>
+        <div key={i} style={{background:"#fff",margin:"20px 0",padding:25,borderLeft:"12px solid blue",borderRadius:10}}>
+          <div style={{fontSize:36,fontWeight:"bold"}}>{t.unidade}</div>
+          <div style={{fontSize:32,marginTop:10,fontWeight:"600"}}>{t.tarefa}</div>
+          <div style={{fontSize:28,marginTop:10}}>{t.tecnico || "-"}</div>
+          <div style={{background:cores[t.status],color:"#fff",padding:15,marginTop:15,fontSize:26,fontWeight:"bold",borderRadius:8,textAlign:"center"}}>
             {t.status}
           </div>
         </div>
